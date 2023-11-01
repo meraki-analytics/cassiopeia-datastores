3d706e99cce351b9e1354e91ce3b674d04cb463f
Ethan Henderson <ethan@zbee.codes>
2023-10-31 18:18:01 -0600

    Fix key-change during iteration if expirations are set in python 3.12
    Signed-off-by: Ethan Henderson <ethan@zbee.codes>

--- cassiopeia-diskstore/cassiopeia_diskstore/common.py
+++ cassiopeia-diskstore/cassiopeia_diskstore/common.py
@@ -32,7 +32,7 @@ class SimpleKVDiskService(DataSource, DataSink):
             os.mkdir(path)
         self._store = simplekv.fs.FilesystemStore(path)
         self._expirations = dict(expirations) if expirations is not None else self._default_expirations
-        for key, value in self._expirations.items():
+        for key, value in self._expirations.copy().items():
             if isinstance(key, str):
                 new_key = globals()[key]
                 self._expirations[new_key] = self._expirations.pop(key)
